b773188e4839d0f1ea2b4bc003687ba55ee73166
shimmyshine <jefflhuntjr@aol.com>
2021-12-17 22:25:59 -0600

    commit

--- src/chains/networkRouter.ts
+++ src/chains/networkRouter.ts
@@ -22,6 +22,8 @@ const NetworkRouter = async (
 
   const provider = getProvider(networkSettings.name);
   const signer = getSigner(networkSettings.name, provider);
+  await provider.ready;
+
   const systemGas = {
     gasPrice:
       networkSettings.gasPriceEnforced == 0
@@ -32,38 +34,25 @@ const NetworkRouter = async (
   /* Blocknumber */
   if (networkSettings.showBlockNumber) {
     setInterval(async () => {
-      try {
-        log.info(
-          networkSettings.name + ": Block: " + (await getBlock(provider)),
-        );
-      } catch (e) {
-        log.warn(e);
-      }
+      log.info(networkSettings.name + ": Block: " + (await getBlock(provider)));
     }, networkSettings.blockNumberFreq);
   }
 
   groups.map((grp, i: number) => {
-    const interVal = setInterval(() => {
-      Object.values(order[grp]).map(async (res, z: number) => {
-        let specificFunctionData = null;
-        let clearToProceed = false;
-        try {
-          specificFunctionData = await getFunctionByID(res);
-        } catch (e) {
-          log.warn(e);
-        }
-        clearToProceed = true;
+    const interVal = setInterval(
+      () => {
+        Object.values(order[grp]).map(async (res, z: number) => {
+          const specificFunctionData = await getFunctionByID(res);
 
-        if (clearToProceed && specificFunctionData != null) {
-          // eslint-disable-next-line @typescript-eslint/no-var-requires
-          const functionSettings = require("../." +
-            specificFunctionData[res].directory +
-            "/settings.ts");
+          if (specificFunctionData != null) {
+            // eslint-disable-next-line @typescript-eslint/no-var-requires
+            const functionSettings = require("../." +
+              specificFunctionData[res].directory +
+              "/settings.ts");
 
-          if (functionSettings.default.active) {
-            try {
+            if (functionSettings.default.active) {
               // eslint-disable-next-line @typescript-eslint/no-var-requires
-              await require("../." +
+              require("../." +
                 specificFunctionData[res].directory +
                 "/functions/main.ts").Main(
                 log,
@@ -76,17 +65,18 @@ const NetworkRouter = async (
                   i + 1 + ":" + (z + 1) + ":" + res,
                 ),
               );
-            } catch (e) {
-              log.warn(e);
             }
           }
-        }
-      });
+        });
 
-      if (networkSettings.groupsInterval[grp] === 0) {
-        clearInterval(interVal);
-      }
-    }, networkSettings.groupsInterval[grp]);
+        if (networkSettings.groupsInterval[grp] === 0) {
+          clearInterval(interVal);
+        }
+      },
+      networkSettings.groupsInterval[grp] > 0
+        ? networkSettings.groupsInterval[grp]
+        : 0,
+    );
   });
 };
 
--- src/index.ts
+++ src/index.ts
@@ -15,27 +15,13 @@ const log: Logger = new Logger({
 const main = async (): Promise<void> => {
   log.info("Program started.\n");
 
-  let functions = null;
-  try {
-    functions = await getFunctions();
-  } catch (e) {
-    log.warn(e);
-  }
+  const functions = getFunctions();
 
-  settingsCheck(log, functions ? functions : {});
+  settingsCheck(log, await functions);
 
   Object.values(settings.networks).map(async (res) => {
     if (res.isActive) {
-      try {
-        await NetworkRouter(
-          log,
-          settings.networks[res.name],
-          res.groups,
-          res.orders,
-        );
-      } catch (e) {
-        log.warn(e);
-      }
+      NetworkRouter(log, settings.networks[res.name], res.groups, res.orders);
     }
   });
 };

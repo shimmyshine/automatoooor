7483008172f685f598fc8edda56ea5c446c6e113
shimmyshine <jefflhuntjr@aol.com>
2021-12-22 02:27:56 -0600

    Update prepare-settings.js

--- scripts/prepare-settings.js
+++ scripts/prepare-settings.js
@@ -2,7 +2,7 @@ const glob = require("glob");
 const path = require("path");
 const fs = require("fs");
 
-glob("./src/**/**settings.example.ts", function (_, matches) {
+glob("./src/**/*settings.example.ts", function (_, matches) {
   for (const file of matches) {
     const base = path.dirname(file);
     if (!fs.existsSync(`${base}/settings.ts`)) {

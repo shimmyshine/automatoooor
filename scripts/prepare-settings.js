12798086e96b66bc668a9fb5fa95fb49245ea677
shimmyshine <jefflhuntjr@aol.com>
2021-12-22 02:20:22 -0600

    docker fix

--- scripts/prepare-settings.js
+++ scripts/prepare-settings.js
@@ -1,6 +1,6 @@
-import { glob } from "glob";
-import { path } from "path";
-import { fs } from "fs";
+const glob = require("glob");
+const path = require("path");
+const fs = require("fs");
 
 glob("./src/**/settings.example.ts", function (_, matches) {
   for (const file of matches) {

f7a6c9ebce7024d073d773faaf096052564e7b1f
shimmyshine <jefflhuntjr@aol.com>
2021-12-22 01:59:29 -0600

    print list of active modules at program start

---
+++ src/helpers/printModules.ts
@@ -0,0 +1,7 @@
+import { Logger } from "tslog";
+import { Modules } from "./Interfaces";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+const printModules = (log: Logger, functions: Modules): void => {};
+
+export default printModules;

a1127f524a8c920a1ab4e505fa8997566a2907cd
shimmyshine <jefflhuntjr@aol.com>
2022-01-13 22:43:16 -0600

    Safety Check
    Make sure that the group has 1 or more modules assigned to it.

--- src/chains/networkRouter.ts
+++ src/chains/networkRouter.ts
@@ -57,104 +57,107 @@ const NetworkRouter = async (
   }
 
   for (const grp of groups) {
-    const interVal = setIntervalAsync(
-      async () => {
-        let z = 1;
-        const orderResults: OrderResults = {};
-        for (const modu of Object.values(order[grp])) {
-          if (order[grp][z]) {
-            let specificFunctionData: Modules = {};
-            try {
-              specificFunctionData = await getFunctionByID(modu);
-            } catch (e) {
-              log.warn(e);
-            }
+    if (Object.values(order[grp]).length >= 1) {
+      const interVal = setIntervalAsync(
+        async (): Promise<void> => {
+          let z = 1;
+          const orderResults: OrderResults = {};
 
-            let functionSettings = null;
-            try {
-              functionSettings = await import(
-                "../." + specificFunctionData[modu].directory + "/settings.ts"
-              );
-            } catch (e) {
-              log.warn(e);
-            }
-
-            const otfSettings: OTFSettings = getOTFSettings(
-              networkSettings.name,
-              grp + ":" + z + ":" + modu,
-            );
-
-            let requirePreviousTrue = false;
-
-            try {
-              if (
-                typeof otfSettings !== "undefined" &&
-                typeof otfSettings.requirePreviousTrue !== "undefined"
-              ) {
-                requirePreviousTrue = otfSettings.requirePreviousTrue;
-              }
-            } catch (e) {
-              log.warn(e);
-            }
-
-            if (
-              z >= 1 &&
-              typeof networkSettings.requireAllTrue !== "undefined" &&
-              ((networkSettings.requireAllTrue && !orderResults[z - 1]) ||
-                (requirePreviousTrue && !orderResults[z - 1]))
-            ) {
-              orderResults[z] = false;
-
-              if (specificFunctionData[modu].moduleSettings.showLog) {
-                log.warn(
-                  "[Module: " +
-                    specificFunctionData[modu].moduleName +
-                    "]: The previous module returned false, cancelling this module execution.",
-                );
-              }
-
-              z++;
-
-              break;
-            }
-
-            if (functionSettings.default.active) {
-              let moduleResult;
-
+          for (const modu of Object.values(order[grp])) {
+            if (order[grp][z]) {
+              let specificFunctionData: Modules = {};
               try {
-                moduleResult = await import(
-                  "../." +
-                    specificFunctionData[modu].directory +
-                    "/functions/main.ts"
-                );
-
-                const modResult = await moduleResult.Main(
-                  log,
-                  address,
-                  provider,
-                  signer,
-                  enforcedGas,
-                  otfSettings,
-                );
-
-                orderResults[z] = modResult;
+                specificFunctionData = await getFunctionByID(modu);
               } catch (e) {
                 log.warn(e);
               }
+
+              let functionSettings = null;
+              try {
+                functionSettings = await import(
+                  "../." + specificFunctionData[modu].directory + "/settings.ts"
+                );
+              } catch (e) {
+                log.warn(e);
+              }
+
+              const otfSettings: OTFSettings = getOTFSettings(
+                networkSettings.name,
+                grp + ":" + z + ":" + modu,
+              );
+
+              let requirePreviousTrue = false;
+
+              try {
+                if (
+                  typeof otfSettings !== "undefined" &&
+                  typeof otfSettings.requirePreviousTrue !== "undefined"
+                ) {
+                  requirePreviousTrue = otfSettings.requirePreviousTrue;
+                }
+              } catch (e) {
+                log.warn(e);
+              }
+
+              if (
+                z >= 1 &&
+                typeof networkSettings.requireAllTrue !== "undefined" &&
+                ((networkSettings.requireAllTrue && !orderResults[z - 1]) ||
+                  (requirePreviousTrue && !orderResults[z - 1]))
+              ) {
+                orderResults[z] = false;
+
+                if (specificFunctionData[modu].moduleSettings.showLog) {
+                  log.warn(
+                    "[Module: " +
+                      specificFunctionData[modu].moduleName +
+                      "]: The previous module returned false, cancelling this module execution.",
+                  );
+                }
+
+                z++;
+
+                break;
+              }
+
+              if (functionSettings.default.active) {
+                let moduleResult;
+
+                try {
+                  moduleResult = await import(
+                    "../." +
+                      specificFunctionData[modu].directory +
+                      "/functions/main.ts"
+                  );
+
+                  const modResult = await moduleResult.Main(
+                    log,
+                    address,
+                    provider,
+                    signer,
+                    enforcedGas,
+                    otfSettings,
+                  );
+
+                  orderResults[z] = modResult;
+                } catch (e) {
+                  log.warn(e);
+                }
+              }
+
+              z++;
             }
-
-            z++;
           }
-        }
 
-        if (networkSettings.groupsInterval[grp] === 0) {
-          clearIntervalAsync(interVal);
-        }
-      },
-      networkSettings.groupsInterval[grp] > 0
-        ? networkSettings.groupsInterval[grp]
-        : 10,
-    );
+          if (networkSettings.groupsInterval[grp] === 0) {
+            clearIntervalAsync(interVal);
+          }
+        },
+        networkSettings.groupsInterval[grp] > 0
+          ? networkSettings.groupsInterval[grp]
+          : 10,
+      );
+    }
   }
 };
 

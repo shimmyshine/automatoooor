e55d2ee1bb315ec2572e4fd3b8f1cfb54823eaf6
shimmyshine <jefflhuntjr@aol.com>
2022-01-17 11:30:42 -0600

    remove noncemanager

--- src/helpers/getSigner.ts
+++ src/helpers/getSigner.ts
@@ -1,10 +1,9 @@
 import { BaseProvider } from "@ethersproject/providers";
 import { ethers, Signer } from "ethers";
 import { getPK } from "./getAddress";
-import { NonceManager } from "@ethersproject/experimental";
 
 export const getSigner = (network: string, provider: BaseProvider): Signer => {
   const signer = new ethers.Wallet(getPK(network), provider);
 
-  return new NonceManager(signer);
+  return signer;
 };

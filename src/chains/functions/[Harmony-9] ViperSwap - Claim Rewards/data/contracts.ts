3ef2c5503a396cd2df5f69128eded8f95e63863d
shimmyshine <jefflhuntjr@aol.com>
2022-02-11 10:14:38 -0600

    Include community pools & vipernests

--- src/chains/functions/[Harmony-9] ViperSwap - Claim Rewards/data/contracts.ts
+++ src/chains/functions/[Harmony-9] ViperSwap - Claim Rewards/data/contracts.ts
@@ -3,4 +3,11 @@ import { Contracts } from "./interfaces";
 export const contracts: Contracts = {
   viperAddress: "0xea589e93ff18b1a1f1e9bac7ef3e86ab62addc79",
   masterBreederAddress: "0x7AbC67c8D4b248A38B0dc5756300630108Cb48b4",
+  xViperTowsWAGMIAddress: "0x88b0daaef8e729d415d8aa502915527a9425878c",
+  wsWAGMIToViperAddress: "0x249a360cec6687e145d76444af176335f7c2f818",
+  xViperToViperAddress: "0x75de75158b0ce2fe38eaf61632f96ecd713a6ff7",
+  xViperToCSHAREAddress: "0x5d36de2846134e6bac2e84042c975e460337538d",
+  immortlOneToImmortlAddress: "0xe46d04bbda016d15011104b402acafcad2a263fe",
+  immortlViperToImmortlAddress: "0x979d63373e2078ca53ea6602bc7327544b3f89f4",
+  hplayOneToHPLAYAddress: "0x7a234cc13d03e9b9cc1abeeda29b0977855a1e99",
 };
--- src/chains/functions/[Harmony-9] ViperSwap - Claim Rewards/data/interfaces.ts
+++ src/chains/functions/[Harmony-9] ViperSwap - Claim Rewards/data/interfaces.ts
@@ -1,6 +1,13 @@
 export interface Contracts {
   viperAddress: string;
   masterBreederAddress: string;
+  xViperTowsWAGMIAddress: string;
+  wsWAGMIToViperAddress: string;
+  xViperToViperAddress: string;
+  xViperToCSHAREAddress: string;
+  immortlOneToImmortlAddress: string;
+  immortlViperToImmortlAddress: string;
+  hplayOneToHPLAYAddress: string;
 }
 
 export interface OTFSettings {

6875a5865152dccaee00b499afe6d98a9dff7f30
shimmyshine <jefflhuntjr@aol.com>
2022-02-13 23:45:08 -0600

    xViper - Enter or Withdraw - Index Updates

--- src/chains/functions/[Harmony-8] ViperSwap - Enter or Withdraw xViper/index.ts
+++ src/chains/functions/[Harmony-8] ViperSwap - Enter or Withdraw xViper/index.ts
@@ -1,9 +1,10 @@
 import settings from "./settings";
 
 const moduleInfo = {
-  moduleID: 0,
-  moduleName: "Module Name",
-  moduleDescription: "Module Description",
+  moduleID: 8,
+  moduleName: "ViperSwap - Enter or Withdraw xViper",
+  moduleDescription:
+    "Allows you to deposit your viper into the ViperPit or withdraw your viper from the ViperPit.",
   moduleSettings: settings,
   moduleSettingsCheck: "settingsCheck.ts",
   chain: "Harmony", // Harmony, Fantom, Binance_Smart_Chain, Ethereum, Avalanche, Aurora, Polygon, RSK, Telos

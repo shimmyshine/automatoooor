7ff62f8f5ab44f8d9034db96e12cd754e8770d99
Isaiah <50631336+DrMonkeyturds@users.noreply.github.com>
2022-02-13 16:26:36 -0800

    add module

---
+++ src/chains/functions/[BSC-44] - Sushiswap Perform Swap/data/contracts.ts
@@ -0,0 +1,6 @@
+import { Contracts } from "./interfaces";
+
+export const contracts: Contracts = {
+  sushiSwapRouter: "0x1b02da8cb0d097eb8d57a175b88c7d8b47997506",
+  wONE: "0xcF664087a5bB0237a0BAd6742852ec6c8d69A27a",
+};
